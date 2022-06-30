d3d60b4f728acc2dd44a94f7a3badffd5d40fbec
zz <1871490331@qq.com>
2022-07-01 00:15:41 +0800

    完成了编辑功能

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app">
     <div>
       <span>姓名:</span>
-      <input type="text" v-model="name" />
+      <input type="text" v-model.trim="name" />
     </div>
     <div>
       <span>年龄:</span>
-      <input type="number" v-model="age" />
+      <input type="number" v-model.trim="age" />
     </div>
     <div>
       <span>性别:</span>
@@ -34,7 +34,7 @@
           <td>{{ item.sex }}</td>
           <td>
             <button @click="delFn(index)">删除</button>
-            <button @click="active">编辑</button>
+            <button @click="edit(index)">编辑</button>
           </td>
         </tr>
       </table>
@@ -44,16 +44,42 @@
 <script>
 export default {
   data() {
-    return { name: "", age: "", sex: "", arr: [], obj: {} };
+    return {
+      name: "",
+      age: "",
+      sex: "",
+      arr: [],
+      obj: {},
+      isShow: false,
+      index1: 0,
+    };
   },
   methods: {
     addFn() {
-      this.obj = { name: this.name, age: this.age, sex: this.sex };
-      this.arr.push(this.obj);
-      (this.name = ""), (this.age = "");
+      if (this.name.trim().length === 0 || this.age.trim().length === 0) {
+        alert("请输入有效值");
+        return;
+      }
+      if (this.isShow) {
+        // console.log(this.index1);
+        // console.log();
+        this.arr[this.index1].name = this.name;
+        this.arr[this.index1].age = this.age;
+        this.arr[this.index1].sex = this.sex;
+      } else {
+        this.obj = { name: this.name, age: this.age, sex: this.sex };
+        this.arr.push(this.obj);
+        (this.name = ""), (this.age = "");
+      }
     },
     delFn(ind) {
       this.arr.splice(ind, 1);
+    },
+    edit(ind) {
+      this.isShow = true;
+      this.index1 = ind;
+      this.name = this.arr[ind].name;
+      this.age = this.arr[ind].age;
     },
   },
 };
