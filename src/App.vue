edc536c49fa31eb48bbe7a2da32dcbb59361c587
zz <1871490331@qq.com>
2022-06-30 19:04:39 +0800

    完成了1.2_vue基础 v-for更新监测

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <template>
-  <div id="app">
-    <!-- 语法1:
-      v-for="(值变量名, 索引变量名) in 目标结构"
-      口诀: 想要谁循环就放到谁身上
-     -->
-    <p v-for="(item, index) in arr" :key="index">
-      第{{ index + 1 }}个学生的名字是：{{ item }}
-    </p>
-
-    <p>学生详细信息</p>
+  <div>
     <ul>
-      <li v-for="item in stuArr" :key="item.id">
-        {{ item.name }} -- {{ item.sex }}-- {{ item.hobby }}
+      <li v-for="(val, index) in arr" :key="index">
+        {{ val }}
       </li>
     </ul>
-
-    <!-- v-for遍历对象(了解) -->
-    <p>老师信息</p>
-    <ul>
-      <li v-for="item in tObj" :key="item">
-        <span>{{ item }}</span>
-        <span></span>
-        <span></span>
-      </li>
-    </ul>
-
-    <div></div>
-
-    <!-- v-for遍历整数(了解) - 从1开始 -->
-    <p>序号</p>
-    <div v-for="item in count" :key="item">{{ item }}</div>
+    <button @click="revBtn">数组翻转</button>
+    <button @click="sliceBtn">截取前3个</button>
+    <button @click="updateBtn">更新第一个元素值</button>
   </div>
 </template>
 
@@ -38,34 +16,26 @@ export default {
   name: "Practise3App",
 
   data() {
-    return {
-      arr: ["小明", "小欢欢", "大黄"],
-      stuArr: [
-        {
-          id: 1001,
-          name: "孙悟空",
-          sex: "男",
-          hobby: "吃桃子",
-        },
-        {
-          id: 1002,
-          name: "猪八戒",
-          sex: "男",
-          hobby: "背媳妇",
-        },
-      ],
-      tObj: {
-        name: "小黑",
-        age: 18,
-        class: "1期",
-      },
-      count: 10,
-    };
+    return { arr: [5, 3, 9, 2, 1] };
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    revBtn() {
+      this.arr.reverse();
+    },
+    sliceBtn() {
+      this.arr = this.arr.splice(0, 3);
+    },
+    updateBtn() {
+      // 更新第一个元素值，2种方法
+      // 1：直接赋值覆盖
+      // this.arr = [100, 3, 9, 2, 1];
+      // 2:利用vue的$set()方法修改制定位置的值
+      this.$set(this.arr, 0, 1000);
+    },
+  },
 };
 </script>
 
